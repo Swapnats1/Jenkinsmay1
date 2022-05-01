124691dbb312f44d2ba1e86c7a7b06dfb0356bd4
EC2 Default User <ec2-user@ip-172-31-27-229.us-east-2.compute.internal>
2022-05-01 22:01:47 +0000

    updated

--- test.sh
+++ test.sh
@@ -1,9 +1,5 @@
 public class Main {
-  public static void main(String[] args) {
-<<<<<<< HEAD
-    System.out.println("Wellcom");
-=======
-    System.out.println("Helloworld");
->>>>>>> 8d8825d9e5ed071380719a5d710b799e1e7f1f20
+  public static void main(String[] D
+    System.out.println("Wellcome")
   }
 }
